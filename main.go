41a372e56ea46c35d49d59f85d3a7c3fca3579f6
abakarmagomedov <amagomedov@smedialink.com>
2018-02-16 18:50:10 +0300

    something new

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	"io"
 	"net/http"
+	"fmt"
+	"github.com/satori/go.uuid"
+	"sync"
+	"io"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -10,6 +13,34 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8000", nil)
+	http.Handle("/hello", helloWorldHandler{})
+	http.Handle("/secureHello", authenticate(helloWorldHandler{}))
+	http.HandleFunc("/login", handleLogin)
+
+	http.ListenAndServe(":3000", nil)
 }
+
+const loginPage = "<html><head><title>Login</title></head><body><form action=\"login\" method=\"post\"> <input type=\"password\" name=\"password\" /> <input type=\"submit\" value=\"login\" /> </form> </body> </html>"
+
+
+type helloWorldHandler struct {
+}
+
+func (h helloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World!!!")
+}
+
+type authenticationMiddleware struct {
+	wrappedHandler http.Handler
+}
+
+func (h authenticationMiddleware) ServeHTTP(w http.ResponseWriter,r *http.Request) {
+}
+
+func authenticate(h http.Handler) authenticationMiddleware {
+	return authenticationMiddleware{h}
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+}
+
