39f8bfd2c8dc3d2eb74c79adb8905d25da846e93
abakarmagomedov <amagomedov@smedialink.com>
2018-07-05 19:39:53 +0300

    some tests are written

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+
+	r := mux.NewRouter();
+	r.HandleFunc("/hello", handler).Methods("GET")
+	http.ListenAndServe(":8080", r)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
