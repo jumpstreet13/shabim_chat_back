0c5c6a57a7a19ce1457994da346c7d808d4f918b
abakarmagomedov <amagomedov@smedialink.com>
2018-07-05 19:17:47 +0300

    working with directories

--- main.go
+++ main.go
@@ -3,44 +3,13 @@ package main
 import (
 	"net/http"
 	"fmt"
-	"github.com/satori/go.uuid"
-	"sync"
-	"io"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello World")
-}
-
 func main() {
-	http.Handle("/hello", helloWorldHandler{})
-	http.Handle("/secureHello", authenticate(helloWorldHandler{}))
-	http.HandleFunc("/login", handleLogin)
-
-	http.ListenAndServe(":3000", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
-const loginPage = "<html><head><title>Login</title></head><body><form action=\"login\" method=\"post\"> <input type=\"password\" name=\"password\" /> <input type=\"submit\" value=\"login\" /> </form> </body> </html>"
-
-
-type helloWorldHandler struct {
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Roma likes rugby!")
 }
-
-func (h helloWorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello World!!!")
-}
-
-type authenticationMiddleware struct {
-	wrappedHandler http.Handler
-}
-
-func (h authenticationMiddleware) ServeHTTP(w http.ResponseWriter,r *http.Request) {
-}
-
-func authenticate(h http.Handler) authenticationMiddleware {
-	return authenticationMiddleware{h}
-}
-
-func handleLogin(w http.ResponseWriter, r *http.Request) {
-}
-
